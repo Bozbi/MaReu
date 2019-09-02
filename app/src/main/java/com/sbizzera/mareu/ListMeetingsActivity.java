85c58def4afe1e7cab5afcbd43b9a47b9b6803dc
bosbizz <bosbizz@gmail.com>
2019-09-02 21:16:55 +0200

    Restart from scratch

--- app/src/main/java/com/sbizzera/mareu/ListMeetingsActivity.java
+++ app/src/main/java/com/sbizzera/mareu/ListMeetingsActivity.java
@@ -1,32 +1,16 @@
 package com.sbizzera.mareu;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProviders;
 
 public class ListMeetingsActivity extends AppCompatActivity {
-
-    private ListMeetingsViewModel mListMeetingsViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_meetings);
-
-
-
-        mListMeetingsViewModel = ViewModelProviders.of(this).get(ListMeetingsViewModel.class);
-
-
+        
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.list_meetings_menu,menu);
-        return true;
-    }
 }
